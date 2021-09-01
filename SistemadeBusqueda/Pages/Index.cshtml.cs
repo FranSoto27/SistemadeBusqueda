56968f5f412f7a6116a16575eac25bf46983fa12
FRANCISCA FERNADA SOTO SEPULVEDA <FRANCISCA.SOTOS@correoaiep.cl>
2021-09-01 16:35:21 -0400

    Clase 20210823 - conexion a la base de datos y validacion de usuarios  y campos

--- SistemadeBusqueda/Pages/Index.cshtml.cs
+++ SistemadeBusqueda/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SistemadeBusqueda.NewFolder;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace SistemadeBusqueda.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required(ErrorMessage = "El campo Usuario es requerido")]
+        public string Usuario { get; set; }
+        [Display(Name="Constraseña")]
+        [BindProperty]
+        [Required(ErrorMessage = "El campo Contraseña es requerido")]
+        public string Password { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -20,6 +30,31 @@ namespace SistemadeBusqueda.Pages
         public void OnGet()
         {
 
+        }
+        public ActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = this.Usuario;
+                var pass = this.Password;
+                var repo = new IndexRepository();
+
+                if (repo.ValidarUsuario(usuario, pass)) 
+                {
+                    return RedirectToPage("./Prueba");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "El usuario o la contraseña no son válidos");
+                    return Page();
+                }
+
+
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
